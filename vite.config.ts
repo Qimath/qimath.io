650a52e5fc10edbab0017b3186aab48661cec759
Qimath <vibert.christopher@gmail.com>
2023-02-26 16:01:43 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://qimath.github.io/qimath.io/',
+  base: 'https://qimath.io/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
